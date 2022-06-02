a7f21adea9eac5c84ffaa4be69da82f79a64eb07
haiderthon <105380566+haiderthon@users.noreply.github.com>
2022-06-03 02:44:57 +0300

    Initial commit

---
+++ exampleconfig.py
@@ -0,0 +1,17 @@
+from sample_config import Config
+
+
+class Development(Config):
+# ملاحضه تمسح الكلام العربي وتضع ببدالة الكود
+    APP_ID = 6
+    API_HASH = " هاش"
+    ALIVE_NAME = "ع اسمك"
+    DB_URI = "ضع رابط التخزين"
+    STRING_SESSION = "ضع كود تريمكس"
+    TG_BOT_TOKEN = "ضع توكن بوتك"
+    TG_BOT_USERNAME = "ضع معرف بوتك"
+    PRIVATE_GROUP_BOT_API_ID = -100
+    COMMAND_HAND_LER = "."
+    SUDO_COMMAND_HAND_LER = "."
+    TZ = "Asia/Baghdad"
+# تليثون العرب - @IQTHON
